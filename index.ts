4b87d8d89ff2b2c0b4829d9b453202c8d42e9523
Gedeon Sainrival <hello@gedeondoescode.com>
2024-10-14 21:05:50 -0400

    add functionality & ping command

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import {
+  Client,
+  CommandInteraction,
+  Events,
+  GatewayIntentBits,
+  type Interaction,
+} from "discord.js";
+import { deployCommands } from "./deploy-commands";
+import { commands } from "./commands";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, async (c) => {
+  await deployCommands();
+  console.log(`${c.user.tag} is ready!`);
+});
+
+client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+
+  const commandInteraction = interaction as CommandInteraction;
+
+  const { commandName } = commandInteraction;
+  if (commands[commandName as keyof typeof commands]) {
+    commands[commandName as keyof typeof commands].execute(commandInteraction);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
